25ff45071db835e6a3dbfc342daae46a34ec3f22
b1zz1 <eduardobizzi2005@gmail.com>
2022-10-10 08:54:23 -0300

    Data storage

--- JS/script.js
+++ JS/script.js
@@ -5,9 +5,26 @@ const openOrderModal = document.querySelectorAll("[data-open]")
 const closeOrderModal = document.querySelectorAll("[data-close]")
 const isVisible = "is-visible"
 const pizzaSize = document.getElementsByName('psize')
-const displayPizzaSize = document.querySelector("#psize-input")
+
+function getValues(){
+  const psizeGetter = document.getElementsByName("psize")
+  const extrasGetter= document.getElementsByName("extra")
+  let size, extra;
+  psizeGetter.forEach(element => {
+    if(element.checked) size = element.value
+  });
+  extrasGetter.forEach(element => {
+    if(element.checked) extra = element.value
+  });
+  const order = {
+    size: size,
+    extra: extra
+  }
+  localStorage.setItem("orderValues", size,extra)
+}
 
 function panelChange(){
+    getValues()
     hideMenuPanel.style.display = 'none'; //esconder o painel de pedido
     showAddressPanel.style.display = 'block'; //mostrar o painel de endereço
     titleChange.innerHTML = "ENDEREÇO"; //mudar o título da página de 'pedido para 'endereço'
@@ -25,12 +42,12 @@ for (const el of closeOrderModal) {
     el.addEventListener("click", function() {
       this.parentElement.parentElement.parentElement.classList.remove(isVisible);
     });
-  }
+}
 
-  function pizzaSizeDisplay(){
-    for(i = 0; i < pizzaSize.length; i++) {
-      if (pizzaSize[i].checked) {
-          displayPizzaSize.innerHTML = pizzaSize[i].value;
-      }
-    }
+function setModalValues(){
+  const order = localStorage.getItem("orderValues")
+  const pizzaSize = document.getElementById("psize-value")
+  const orderExtras = document.getElementById("order-extra-input")
+  pizzaSize.innerText = order
+  orderExtras.innerText = order
 }
